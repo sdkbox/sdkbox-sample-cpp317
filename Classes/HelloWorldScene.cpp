f8d2d04b06ebd5f561b1ff4b64b7a0465b8e9b0c
hugo <hugohuang1111@gmail.com>
2018-08-23 10:42:57 +0800

    add unityads plugin

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -25,7 +25,85 @@
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
 
+#include "PluginUnityAds/PluginUnityAds.h"
+
 USING_NS_CC;
+
+
+
+
+/******************
+ * Show logs
+ ******************/
+std::vector<std::string> msgbuf;
+static void showMsg(const std::string& msg) {
+    //
+    Label *label = dynamic_cast<Label*>(Director::getInstance()->getNotificationNode());
+    if (label == nullptr) {
+        auto size = Director::getInstance()->getWinSize();
+        label = Label::createWithSystemFont("test", "arial", 16);
+        label->setAnchorPoint(Vec2(0,0));
+        label->setTextColor(Color4B(0, 255, 0, 255));
+        label->setPosition(10, size.height*0.1);
+        Director::getInstance()->setNotificationNode(label);
+    }
+    
+    msgbuf.push_back(msg);
+    if (msgbuf.size() > 10) {
+        msgbuf.erase(msgbuf.cbegin());
+    }
+    
+    
+    std::string text = "";
+    for (int i = 0; i < msgbuf.size(); i++) {
+        std::stringstream buf;
+        buf << i << " " << msgbuf[i] << "\n";
+        text = text + buf.str();
+    }
+    
+    label->setString(text);
+}
+
+/******************
+ * UnityAds Listener
+ ******************/
+
+class UAListener : public sdkbox::UnityAdsListener {
+public:
+    
+    void unityAdsDidClick(const std::string& placementId) {
+        showMsg("unityAdsDidClick");
+    }
+    
+    void unityAdsPlacementStateChanged(const std::string& placementId, sdkbox::PluginUnityAds::SBUnityAdsPlacementState oldState, sdkbox::PluginUnityAds::SBUnityAdsPlacementState newState) {
+        showMsg("unityAdsPlacementStateChanged");
+    }
+    
+    void unityAdsReady(const std::string& placementId) {
+        showMsg("unityAdsReady");
+    }
+    
+    void unityAdsDidError(sdkbox::PluginUnityAds::SBUnityAdsError error, const std::string& message) {
+        showMsg("unityAdsDidError");
+    }
+    
+    void unityAdsDidStart(const std::string& placementId) {
+        showMsg("unityAdsDidStart");
+    }
+    
+    void unityAdsDidFinish(const std::string& placementId, sdkbox::PluginUnityAds::SBUnityAdsFinishState state) {
+        showMsg("unityAdsDidFinish");
+    }
+    
+};
+
+
+
+
+
+
+
+
 
 Scene* HelloWorld::createScene()
 {
@@ -49,72 +127,11 @@ bool HelloWorld::init()
         return false;
     }
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+//    auto visibleSize = Director::getInstance()->getVisibleSize();
+//    Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+    createTestMenu();
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0)
-    {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
     return true;
 }
 
@@ -135,3 +152,31 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
 
 
 }
+
+
+
+void HelloWorld::createTestMenu() {
+    sdkbox::PluginUnityAds::setListener(new UAListener());
+    sdkbox::PluginUnityAds::init();
+    
+    if (!sdkbox::PluginUnityAds::isSupported()) {
+        showMsg("UnityAds is not support this platform");
+        return;
+    }
+    
+    auto menu = Menu::create();
+    
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Show", "arial", 24), [](Ref*){
+        std::string placement = "";
+        if (sdkbox::PluginUnityAds::isReady(placement)) {
+            sdkbox::PluginUnityAds::show(placement);
+        } else {
+            showMsg("UnityAds is not ready");
+        }
+    }));
+    
+    menu->alignItemsVerticallyWithPadding(10);
+    addChild(menu);
+}
+
+
